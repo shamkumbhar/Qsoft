4a8c78ce85505cca7363038ad8ed95df244011c5
shamkumbhar <shamkumbhar47@gmail.com>
2025-06-18 15:19:04 +0530

    Delete frontend/dist directory

--- frontend/dist/js/map.js
+++
@@ -1,5 +0,0 @@
-// Shared JavaScript functions can go here
-document.addEventListener('DOMContentLoaded', function() {
-    // Any shared initialization code
-    console.log('GeoVision application loaded');
-});
